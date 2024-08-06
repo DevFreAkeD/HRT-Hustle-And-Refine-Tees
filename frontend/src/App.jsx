d61973a29de0c5e941a39dc103c0998ef098491b
Tushar Nagar <tusharrock146@gmail.com>
2024-08-06 09:38:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Header from './components/main/Header'
+import React from 'react';
+import Header from './components/Header';
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default App
+export default App;
