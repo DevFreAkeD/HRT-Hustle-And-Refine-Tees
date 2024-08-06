a94231de7658a19643ce60cde58918092dd3a017
Tushar Nagar <tusharrock146@gmail.com>
2024-08-06 09:41:53 +0530

    Create Carousel.jsx

---
+++ frontend/src/components/main/Carousel.jsx
@@ -0,0 +1,15 @@
+// src/components/Carousel.js
+
+import React from 'react';
+
+const Carousel = () => {
+  return (
+    <div className="carousel w-full h-[30rem] bg-gray-300">
+      <div className="flex justify-center">
+        <img src="https://via.placeholder.com/800x300" alt="Carousel Slide" className="w-auto h-[30rem]" />
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
