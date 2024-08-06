939b4773b05df236cca1a7dfb94c7edffa9e77fa
Tushar Nagar <tusharrock146@gmail.com>
2024-08-06 09:45:37 +0530

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from './main/Carousel';
+import CategoryGrid from './main/CategoryGrid';
 
 const Main = () => {
   return (
     <div>
       <Carousel />
+      <CategoryGrid />
     </div>
   );
 };
