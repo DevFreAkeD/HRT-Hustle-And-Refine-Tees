4491ad54be7b6b1c54741ce256a30f39a0d564a3
Tushar Nagar <tusharrock146@gmail.com>
2024-08-06 13:15:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import connectDB from './mongodb/connect.js';
+import adminRoutes from './routes/adminRoutes.js';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+// Use admin routes
+app.use('/api/admin', adminRoutes);
+
+app.get('/', (req, res) => {
   res.send('Hello from Hustle & Refine Tees!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error stack
+  res.status(500).json({ message: 'Something went wrong!' });
 });
 
 const startServer = async () => {
@@ -18,7 +29,7 @@ const startServer = async () => {
     await connectDB(process.env.MONGODB_URI); // Ensure the database connection is awaited
     app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
   } catch (error) {
-    console.log(error);
+    console.error('Error starting server:', error);
   }
 };
 
